35d3167be77f24b323277ed4c7964d44e7049543
vinhdothanh912 <vinh.dothanh912@gmail.com>
2024-05-11 14:26:07 +0700

    update photo screen

---
+++ app/(tabs)/photo.tsx
@@ -0,0 +1,62 @@
+import Button from "@/components/Button";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import * as ImagePicker from "expo-image-picker";
+import { StyleSheet, View, Image, Pressable, Text } from "react-native";
+
+const PlaceholderImage = require("../../assets/images/background-image.png");
+
+export default function PhotoScreen() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+});
